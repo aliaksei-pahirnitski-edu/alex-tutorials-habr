932e9d04d09e3096694ab19a2d9220be5ae8602d
aliaksei.pahirnitski.edu <aliaksei.pahirnitski.edu@gmail.com>
2023-10-27 05:25:27 +0300

    finish tutorial

--- Dotnet-Wasm-React/React/myapp/src/App.tsx
+++ Dotnet-Wasm-React/React/myapp/src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useDotNet } from './useDotNet'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { dotnet, loading } = useDotNet('/AppBundle/dotnet.js')
+
+  const fileSelected = async (e: any) => {
+
+    const file = e.target.files[0];
+    const data = new Uint8Array(await file.arrayBuffer());
+
+    const result = await dotnet.FileProcessor.ProcessFile(data)
+
+    alert(`Result: ${result}`);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input disabled={loading} type="file" onChange={fileSelected}></input>
     </>
   )
 }
